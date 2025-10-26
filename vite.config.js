3b4d9e82cd6c44c0eb93056d3e7e8cca6656d072
MaksymSelezen <foqney@gmail.com>
2025-10-26 15:46:21 +0200

    fix bag

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
+  base: "/Finity-Touch/",
   publicDir: "public",
   build: {
     rollupOptions: {
